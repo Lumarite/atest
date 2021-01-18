2fe99703deb6c8ad330c5fd0fa67cd34a079d577
Lumarite <77590345+Lumarite@users.noreply.github.com>
2021-01-17 20:44:45 -0500

    Update questions.js

--- questions.js
+++ questions.js
@@ -3,16 +3,33 @@ questions = [
         "question": "Oppression by corporations is more of a concern than oppression by governments.",
         "effect": {
             "econ": 10,
-            "dipl": 10,
-            "govt": 10,
-            "scty": 10
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
         }
     },
     {
         "question": "It is necessary for the government to intervene in the economy to protect consumers.",
         "effect": {
-            "econ": 10,
+            "econ": 0,
+            "dipl": 10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "It is necessary for government.",
+        "effect": {
+            "econ": 0,
             "dipl": 0,
+            "govt": 0,
+            "scty": -10
+        }
+    },    {
+        "question": "It is necessary for the government to intervene in the economy to protect consumers.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
             "govt": 0,
             "scty": 0
         }
@@ -20,8 +37,8 @@ questions = [
     {
         "question": "The freer the markets, the freer the people.",
         "effect": {
-            "econ": -10,
-            "dipl": 0,
+            "econ": 0,
+            "dipl": -10,
             "govt": 0,
             "scty": 0
         }
